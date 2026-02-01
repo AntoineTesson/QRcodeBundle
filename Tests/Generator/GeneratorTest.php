acbf0722a1b63b70a0e90b8aecca1eb22120a8d6
Antoine Tesson <tesson.a@gmail.com>
2026-02-01 19:48:17 +0100

    Fix tests: make data providers static (required by test framework v11)

--- Tests/Generator/GeneratorTest.php
+++ Tests/Generator/GeneratorTest.php
@@ -7,101 +7,96 @@ use Skies\QRcodeBundle\Generator\Generator;
 
 class GeneratorTest extends TestCase
 {
-    public function getOptions(): array
+    public static function getOptions(): array
     {
-        return array(
-            array(
-                array(
+        return [
+            [
+                [
                     'code'   => '0123456789',
                     'type'   => 'c128',
                     'format' => 'html',
                     'width'  => 2,
                     'height' => 30,
                     'color'  => 'black',
-                ),
-            ),
-            array(
-                array(
+                ],
+            ],
+            [
+                [
                     'code'   => '0123456789',
                     'type'   => 'c39',
                     'format' => 'svg',
-                ),
-            ),
-            array(
-                array(
+                ],
+            ],
+            [
+                [
                     'code'   => '0123456789',
                     'type'   => 'qrcode',
                     'format' => 'png',
                     'width'  => 5,
                     'height' => 5,
-                    'color'  => array(0 ,0, 0),
-                ),
-            ),
-        );
+                    'color'  => [0, 0, 0],
+                ],
+            ],
+        ];
     }
 
     /**
-     * testGenerate
-     *
-     * @medium
-     *
      * @dataProvider getOptions
      */
-    public function testGenerate(array $options = array()): void
+    public function testGenerate(array $options): void
     {
         $generator = new Generator();
 
         $this->assertIsString($generator->generate($options));
     }
 
-    public function getErrorOptions(): array
+    public static function getErrorOptions(): array
     {
-        return array(
-            array(
-                array(
-                    'code'   => '0123456789',
-                ),
-            ),
-            array(
-                array(
+        return [
+            [
+                [
+                    'code' => '0123456789',
+                ],
+            ],
+            [
+                [
                     'code'   => '0123456789',
                     'type'   => 'Unknown Type',
                     'format' => 'html',
-                ),
-            ),
-            array(
-                array(
+                ],
+            ],
+            [
+                [
                     'code'   => '0123456789',
                     'type'   => 'c128',
                     'format' => 'Unknown Format',
-                ),
-            ),
-            array(
-                array(
+                ],
+            ],
+            [
+                [
                     'code'   => '0123456789',
                     'type'   => 'c39',
                     'format' => 'svg',
                     'width'  => 'width is int',
-                ),
-            ),
-            array(
-                array(
+                ],
+            ],
+            [
+                [
                     'code'   => '0123456789',
                     'type'   => 'qrcode',
                     'format' => 'png',
                     'width'  => 5,
                     'height' => 5,
                     'color'  => 5,
-                ),
-            ),
-        );
+                ],
+            ],
+        ];
     }
 
     /**
-     * @medium
      * @dataProvider getErrorOptions
      */
-    public function testConfigureOptions(array $options = array()): void
+    public function testConfigureOptions(array $options): void
     {
         $this->expectException(\Exception::class);
         $generator = new Generator();
